f2765c2d9d390b913482671128bbbc1771b37a2f
Ronith Menneni <ronithmenneni@gmail.com>
2026-04-13 11:52:27 +0530

    q4 implementation

---
+++ q4/q4.c
@@ -0,0 +1,48 @@
+#include <dlfcn.h>
+#include <stdio.h>
+
+int main(void) {
+	char op[6];
+	int a;
+	int b;
+
+	while (1) {
+		int got = scanf("%5s %d %d", op, &a, &b);
+		if (got != 3) {
+			break;
+		}
+
+		char libname[20];
+		char op_copy[6];
+		void *handle;
+		int (*fn)(int, int);
+		int result;
+		int final_result;
+
+		sprintf(op_copy, "%s", op);
+		sprintf(libname, "./lib%s.so", op_copy);
+
+		handle = dlopen(libname, RTLD_LAZY);
+		if (handle == NULL) {
+			continue;
+		}
+
+		fn = (int (*)(int, int))dlsym(handle, op_copy);
+		if (fn == NULL) {
+			dlclose(handle);
+			continue;
+		}
+
+		result = fn(a, b);
+        if (result < 0) {
+            final_result = 0;
+        } else {
+            final_result = result;
+        }
+		printf("%d\n", final_result);
+
+		dlclose(handle);
+	}
+
+	return 0;
+}
